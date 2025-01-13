ef44dc4eafa62daf13df5dfd6512c4de70c4f127
madtofan <ahmadclab@gmail.com>
2025-01-13 10:39:06 +0800

    update metadata

--- src/app/menu/[organizationSlug]/[storeSlug]/layout.tsx
+++ src/app/menu/[organizationSlug]/[storeSlug]/layout.tsx
@@ -2,11 +2,29 @@ import { api } from "~/trpc/server";
 import { type ReactNode } from "react";
 import { MenuProvider } from "./_provider";
 
+type ParamType = Promise<{ organizationSlug: string; storeSlug: string }>;
+
+export async function generateMetadata({ params }: { params: ParamType }) {
+  const { organizationSlug, storeSlug } = await params;
+
+  const menu = await api.store
+    .getStoreMenus({ organizationSlug, storeSlug })
+    .catch(() => {
+      return { name: "", storeMenus: [] };
+    });
+
+  return {
+    title: !menu.name ? "Store not found" : menu.name,
+    description: "POS System for Malaysian Restaurants",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
+  };
+}
+
 export default async function MenuLayout({
   params,
   children,
 }: {
-  params: Promise<{ organizationSlug: string; storeSlug: string }>;
+  params: ParamType;
   children: ReactNode;
 }) {
   const { organizationSlug, storeSlug } = await params;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default async function Home() {
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
                     SwiftPOS: Revolutionize Your Business
                   </h1>
-                  <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                  <p className="mx-auto max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl">
                     Streamline your sales, inventory, and customer management
                     with our cutting-edge Point of Sale system.
                   </p>
@@ -72,7 +72,7 @@ export default async function Home() {
           </section>
           <section
             id="features"
-            className="w-full bg-gray-100 py-12 md:py-24 lg:py-32 dark:bg-gray-800"
+            className="w-full bg-gray-100 py-12 dark:bg-gray-800 md:py-24 lg:py-32"
           >
             <div className="container px-4 md:px-6">
               <h2 className="mb-12 text-center text-3xl font-bold tracking-tighter sm:text-5xl">
@@ -206,7 +206,7 @@ export default async function Home() {
           </section>
           <section
             id="contact"
-            className="w-full bg-gray-100 py-12 md:py-24 lg:py-32 dark:bg-gray-800"
+            className="w-full bg-gray-100 py-12 dark:bg-gray-800 md:py-24 lg:py-32"
           >
             <div className="container px-4 md:px-6">
               <div className="flex flex-col items-center space-y-4 text-center">
@@ -214,7 +214,7 @@ export default async function Home() {
                   <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                     Ready to Get Started?
                   </h2>
-                  <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                  <p className="mx-auto max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                     Transform your business with SwiftPOS. Contact us today for
                     a free demo.
                   </p>
